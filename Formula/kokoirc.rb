79119b3cfb84352f2b9cbb8778cf95dc47665589
kofany <92736603+kofany@users.noreply.github.com>
2026-03-06 23:59:44 +0100

    Update kokoirc to 0.2.6

--- Formula/kokoirc.rb
+++ Formula/kokoirc.rb
@@ -1,8 +1,8 @@
 class Kokoirc < Formula
   desc "Modern terminal IRC client with image preview, SASL, scripting, and theming"
   homepage "https://kofany.github.io/kokoIRC/"
-  url "https://github.com/kofany/kokoIRC/releases/download/v0.2.5/kokoirc-0.2.5-darwin-arm64.tar.gz"
-  sha256 "bb910287cd0b3296b64a74089ef73b9a51fd430e1d862fb076ec622d274111b1"
+  url "https://github.com/kofany/kokoIRC/releases/download/v0.2.6/kokoirc-0.2.6-darwin-arm64.tar.gz"
+  sha256 "5301fe537e140d35e86b70baf27348307a4184161ba8808d13ff8d87610b6bd7"
   license "MIT"
 
   def install
